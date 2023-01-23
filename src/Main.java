4658f72aa8835b2a766c5286695fd099767167a4
Gerasimov Alexandr <gerasimov707@yandex.ru>
2023-01-23 17:24:08 +0300

    первый коммит 2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ import java.util.Scanner;
 public class Main {
 
     public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
-    public static int[] prices = { 50, 135, 65, 53 };
-
-    // В стоимости этих товаров каждые три товара должны стоить как два:
-    public static String[] productsOnSale = { "Хлеб", "Мороженка" };
+    public static int[] prices = { 50, 135, 65 , 53 };
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
@@ -40,23 +37,10 @@ public class Main {
         int sum = 0;
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
-                boolean isOnSale = false;
-                for (String saleProduct : productsOnSale) {
-                    if (products[i].equals(saleProduct)) {
-                        isOnSale = true;
-                    }
-                }
-
-                if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
-                    sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
-                } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
-                    sum += prices[i] * counts[i];
-                }
+                System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                sum += prices[i] * counts[i];
             }
         }
         System.out.println("Итого: " + sum + " руб.");
     }
-
 }
