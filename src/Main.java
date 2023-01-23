9f62449ec8670a63793938aec0a5ca1f369f5206
Gerasimov Alexandr <gerasimov707@yandex.ru>
2023-01-23 17:34:21 +0300

    первый коммит 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
 
     public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
-    public static int[] prices = { 50, 135, 65 , 53 };
+    public static int[] prices = { 50, 135, 65, 53 };
 
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин!");
